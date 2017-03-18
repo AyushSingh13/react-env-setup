59ee9e0b8ce5965dc1fd1f7ecc1177f1fc5b2da6
Ayush Singh <ayush.vijay@gmail.com>
2017-03-18 00:05:18 -0400

    Initial starting react setup

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+/*
+    ./client/components/App.jsx
+*/
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return(
+            <div style={{textAlign: 'center'}}>
+                <h1>Hello React</h1>
+            </div>
+        );
+    }
+}
